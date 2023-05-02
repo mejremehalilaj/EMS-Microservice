f19154321fcfa1199b698d7a4d8ee4b425d5c303
Mejreme Halilaj <111367272+mejremehalilaj@users.noreply.github.com>
2023-05-03 00:17:56 +0200

    Setting up AuthService

---
+++ AuthService.API/Domain/UserCredentials.cs
@@ -0,0 +1,8 @@
+﻿namespace AuthService.API.Domain
+{
+    public class UserCredentials
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
