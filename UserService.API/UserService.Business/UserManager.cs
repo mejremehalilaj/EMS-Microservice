3167cdc91dfa8e83765b31eef139112ad44efb53
Mejreme Halilaj <111367272+mejremehalilaj@users.noreply.github.com>
2023-05-05 00:27:17 +0200

    defining userservice structure

---
+++ UserService.API/UserService.Business/UserManager.cs
@@ -0,0 +1,21 @@
+﻿using UserService.API.Interfaces;
+using UserService.API.Models;
+using UserService.API.UserService.Access;
+
+namespace UserService.API.UserService.Business
+{
+    public class UserManager:IUserManager
+    {
+        private readonly IUser _context;
+
+        public UserManager(IUser _context)
+        {
+            this._context = _context;
+        }
+
+        public async Task<List<UserModel>> FetchUsers()
+        {
+            return await _context.GetUsers();
+        }
+    }
+}
