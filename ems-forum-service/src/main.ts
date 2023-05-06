cb606d35e321d407ae78fb19fb24217d5007d860
gretaisufi <greta.isufi@gmail.com>
2023-05-06 18:11:18 +0200

    feat: add ems forum service with rabbitmq, pg database

---
+++ ems-forum-service/src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
+import { AppModule } from './app.module';
+import { RABBITMQ_URL } from './config/app.config';
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: [RABBITMQ_URL],
+        queue: 'forum_queue',
+      },
+    },
+  );
+
+  await app.listen();
+}
+bootstrap();
