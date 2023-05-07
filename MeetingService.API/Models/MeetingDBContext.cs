7e3ca7b9be33b6a492af53b7f7caaf6e96a19fb3
Agon Hamiti <agon.hamiti5@gmail.com>
2023-05-07 03:59:04 +0200

    Create Meeting and MeetingUser

--- MeetingService.API/Models/MeetingDBContext.cs
+++ MeetingService.API/Models/MeetingDBContext.cs
@@ -16,17 +16,39 @@ namespace MeetingService.API.Models
         {
         }
 
+        public virtual DbSet<MeetingUser> MeetingUsers { get; set; } = null!;
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=AGON\\SQLEXPRESS;Database=MeetingDB;Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer("Server=Agon\\SQLEXPRESS;Database=MeetingDB;Trusted_Connection=True;");
             }
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<MeetingUser>(entity =>
+            {
+                entity.ToTable("MeetingUser");
+
+                entity.Property(e => e.Id)
+                    .HasMaxLength(255)
+                    .IsUnicode(false)
+                    .HasColumnName("id");
+
+                entity.Property(e => e.FirstName)
+                    .HasMaxLength(255)
+                    .IsUnicode(false)
+                    .HasColumnName("firstName");
+
+                entity.Property(e => e.LastName)
+                    .HasMaxLength(255)
+                    .IsUnicode(false)
+                    .HasColumnName("lastName");
+            });
+
             OnModelCreatingPartial(modelBuilder);
         }
 
