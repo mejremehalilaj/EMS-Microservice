e00f3107f50640ed614a39e9a7c2e862e5e566cc
Agon Hamiti <agon.hamiti5@gmail.com>
2023-05-07 05:34:13 +0200

    Connect Database and Creating MeetingController

--- MeetingService.API/Models/MeetingDBContext.cs
+++ MeetingService.API/Models/MeetingDBContext.cs
@@ -16,19 +16,55 @@ namespace MeetingService.API.Models
         {
         }
 
+        public virtual DbSet<Meeting> Meetings { get; set; } = null!;
         public virtual DbSet<MeetingUser> MeetingUsers { get; set; } = null!;
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=Agon\\SQLEXPRESS;Database=MeetingDB;Trusted_Connection=True;");
+/*#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=AGON\\SQLEXPRESS;Database=MeetingDB;Trusted_Connection=True;");*/
             }
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<Meeting>(entity =>
+            {
+                entity.ToTable("Meeting");
+
+                entity.Property(e => e.Id)
+                    .HasMaxLength(255)
+                    .IsUnicode(false)
+                    .HasColumnName("id");
+
+                entity.Property(e => e.Description)
+                    .HasMaxLength(255)
+                    .IsUnicode(false)
+                    .HasColumnName("description");
+
+                entity.Property(e => e.EndTime)
+                    .HasColumnType("datetime")
+                    .HasColumnName("end_time");
+
+                entity.Property(e => e.Online).HasColumnName("online");
+
+                entity.Property(e => e.Place)
+                    .HasMaxLength(255)
+                    .IsUnicode(false)
+                    .HasColumnName("place");
+
+                entity.Property(e => e.StartTime)
+                    .HasColumnType("datetime")
+                    .HasColumnName("start_time");
+
+                entity.Property(e => e.Title)
+                    .HasMaxLength(255)
+                    .IsUnicode(false)
+                    .HasColumnName("title");
+            });
+
             modelBuilder.Entity<MeetingUser>(entity =>
             {
                 entity.ToTable("MeetingUser");
--- MeetingService.API/Program.cs
+++ MeetingService.API/Program.cs
@@ -1,8 +1,15 @@
+using MeetingService.API.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<MeetingDBContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("constring")));
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
