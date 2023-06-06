802843d63fa2cfd0380c4f6b8e9b5090cc5728e4
Mejreme Halilaj <111367272+mejremehalilaj@users.noreply.github.com>
2023-06-06 22:35:13 +0200

    created login & register models

---
+++ UserManagment.Service.API/Models/Authentication/SignUp/RegisterUser.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserManagment.Service.API.Models.Authentication.SignUp
+{
+    public class RegisterUser
+    {
+        [Required(ErrorMessage = "User name is required")]
+        public string? UserName { get; set; }
+
+        [EmailAddress]
+        [Required(ErrorMessage = "Email required")]
+        public string? Email { get; set; }
+        [Required(ErrorMessage = "Password is required")]
+        public string? Password { get; set; }
+
+
+    }
+}
