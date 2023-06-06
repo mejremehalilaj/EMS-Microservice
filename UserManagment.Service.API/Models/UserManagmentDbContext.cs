4ae625a6dce0a051c0645320aeb4fec86519f4f5
Mejreme Halilaj <111367272+mejremehalilaj@users.noreply.github.com>
2023-06-06 22:36:59 +0200

    created user managment context with seed roles

---
+++ UserManagment.Service.API/Models/UserManagmentDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace UserManagment.Service.API.Models
+{
+    public class UserManagmentDbContext:IdentityDbContext<IdentityUser>
+    {
+        public UserManagmentDbContext(DbContextOptions<UserManagmentDbContext>options):base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            this.SeedRoles(builder);
+        }
+
+        private void SeedRoles(ModelBuilder builder)
+        {
+            builder.Entity<IdentityRole>().HasData
+                (
+                new IdentityRole() { Name = "Admin", ConcurrencyStamp = "1", NormalizedName = "Admin" },
+                new IdentityRole() { Name = "User", ConcurrencyStamp = "2", NormalizedName = "User" },
+                new IdentityRole() { Name = "Student", ConcurrencyStamp = "3", NormalizedName = "Student" },
+                new IdentityRole() { Name = "Parent", ConcurrencyStamp = "4", NormalizedName = "Parent" },
+                new IdentityRole() { Name = "Teacher", ConcurrencyStamp = "5", NormalizedName = "Teacher" }
+                );
+        }
+    }
+}
